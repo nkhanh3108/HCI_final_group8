2a689c55503d9409dfd38f6d33c26954d67e3dbb
nkhanh3108 <nkhanh3108@gmail.com>
2019-06-09 04:39:28 +0700

    pushing all after a pull

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         // document.getElementById('menu-user').innerHTML = 'History';
         $('#login-btn').remove();
         // Add logout button vao subscribe-btn
-		$('#subscribe-btn-login').append('<a href="#" style="text-align:center;" id="username-tag">Hello ' + user + '!</a>');
+		$('#subscribe-btn-login').append('<a href="history.html" style="text-align:center;" id="username-tag">Hello ' + user + '!</a>');
 		$('#menu-user').append('<a href="history.html" >History</a>');
 		$('#exit').append( '<a href="login.html" onclick="return exit()" data-toggle="tooltip" data-placement="bottom" title="Log Out"><i class="fa fa-sign-out" aria-hidden="true"></i></a>');
 		
