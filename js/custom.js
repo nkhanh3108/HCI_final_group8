538a12a1f92e749fd4c73794479efcbe23539c4e
nkhanh3108 <51195825+nkhanh3108@users.noreply.github.com>
2019-06-10 03:59:55 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         // Add logout button vao subscribe-btn
 		$('#subscribe-btn-login').append('<a href="history.html" style="text-align:center;" id="username-tag">Hello ' + user + '!</a>');
 		$('#menu-user').append('<a href="history.html" >History</a>');
-		$('#exit').append( '<a href="login.html" onclick="return exit()" data-toggle="tooltip" data-placement="bottom" title="Log Out"><i class="fa fa-sign-out" aria-hidden="true"></i></a>');
+		$('#exit').append( '<a href="index.html" onclick="return exit()" data-toggle="tooltip" data-placement="bottom" title="Log Out"><i class="fa fa-sign-out" aria-hidden="true"></i></a>');
 		
     }else{
 		$('#menu-user').remove;
